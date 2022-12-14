eb313706fa5067fc8cdb11b3befea8734aea60a6
gszemesi <116110036+gszemesi@users.noreply.github.com>
2022-12-14 16:27:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,2 @@
-//elemek kiválasztása
-const azonObj = document.querySelector("#azon");
-const gombObj = document.querySelector("#gomb");
-
-
-const tömb=["asd"];
-
-//kattintás
-gombObj.addEventListener("click",kattintás)
-
-function kattintás(){
-    //beolvasás
-    if(a in tömb){
-        console.log("Hello world!");
-    }
-}
+import data from './data.json';
+console.log(data);
